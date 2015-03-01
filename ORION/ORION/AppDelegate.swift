87632c01c4e2d2ed825e936dd9de5ce39a3b56da
Runemark <martin@runemark.com>
2015-02-28 18:16:41 -0700

    Basic project setup

---
+++ ORION/ORION/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ORION
+//
+//  Created by Martin Mumford on 2/28/15.
+//  Copyright (c) 2015 Runemark Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ORION/ORION/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ORION
+//
+//  Created by Martin Mumford on 2/28/15.
+//  Copyright (c) 2015 Runemark Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
